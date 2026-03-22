33ccacc98076a791211a83c231d807033f047448
Hesh-03 <heshala03@gmail.com>
2026-03-23 01:30:48 +0530

    done  edit profile screen

--- lib/screens/edit_profile_screen.dart
+++ lib/screens/edit_profile_screen.dart
@@ -236,3 +236,4 @@ class _ReadOnlyField extends StatelessWidget {
     ]),
   );
 }
+//edit profile screen by heshala
--- lib/screens/emergency_screen.dart
+++ lib/screens/emergency_screen.dart
@@ -192,3 +192,4 @@ class _ContactCard extends StatelessWidget {
     );
   }
 }
+//emergency screen by heshala
